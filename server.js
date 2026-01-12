60bafc904aedaf22414c784e6d3d53025d8edcc1
VictorAlmeida09 <vlap@aluno.ifnmg.edu.br>
2026-01-12 18:22:22 +0000

    cloudnary

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ cloudinary.v2.config({
 });
 const { Pool } = pkg;
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' })); // aceita até 10MB
 
 let pool = null;
 function conectarBD() {
